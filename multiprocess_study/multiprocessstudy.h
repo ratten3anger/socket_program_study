37a908093f5ea73f026111b012d7fc6491d0a4e2
“o0xmuhe” <o0xmuhe@gmail.com>
2016-10-30 15:25:03 +0800

    head files

--- multiprocess_study/multiprocessstudy.h
+++ multiprocess_study/multiprocessstudy.h
@@ -2,3 +2,17 @@
 #define _MULTIPROCESSSTUDY_H
 #endif
 
+#include <netdb.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <strings.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 1234
+//number of connection
+#define BACKLOG 5
+#define MAXDATASIZE 1000
