e00eeb9a8d972c3362c9216b102f589326ece265
“o0xmuhe” <o0xmuhe@gmail.com>
2016-11-07 16:55:44 +0800

    head file

---
+++ IOreuse_study/IO_reuse.h
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: IO_reuse.h
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 03 Nov 2016 06:34:50 PM CST
+ ************************************************************************/
+
+#ifndef _IO_REUSE_H
+#define _IO_REUSE_H
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <netdb.h>
+
+
+#define PORT 1234
+#define BACKLOG 5
+#define MAXDATASIZE 1000
