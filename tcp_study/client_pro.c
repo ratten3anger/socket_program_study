110b0f7561861b4c351e65550c0a5b05a4f78079
“o0xmuhe” <o0xmuhe@gmail.com>
2016-11-08 17:08:06 +0800

    client also

---
+++ tcp_study/client_pro.c
@@ -0,0 +1,78 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define PORT 1234
+#define MAXDATASIZE 128
+
+
+char *getMessage(char *sendline,int len,FILE *fp){
+	printf("[-]Input string to server :");
+	return(fgets(sendline,MAXDATASIZE,fp));
+}
+
+
+void process(FILE *fp,int sockfd){
+	char sendline[MAXDATASIZE] = {0};
+	char recvline[MAXDATASIZE] = {0};
+	int num;
+	printf("[!]Connect to server\n");
+
+	while(getMessage(sendline,MAXDATASIZE,fp) != NULL){
+		send(sockfd,sendline,strlen(sendline),0);
+		if((num = recv(sockfd,recvline,MAXDATASIZE,0)) == 0){
+			printf("[!]Server terminated\n");
+			return;
+		}
+		recvline[num] = '\0';
+		printf("[*]Server message:%s\n",recvline);
+	}
+	printf("\nExit\n");
+}
+
+
+int main(int argc,char *argv[]){
+
+	int sockfd;
+	struct hostent *he;
+	struct sockaddr_in server;
+	setbuf(stdin,NULL);
+	setbuf(stdout,NULL);
+
+	if(argc != 2){
+		printf("Usage:%s <IP address>\n",argv[0] );
+		exit(1);
+	}
+
+	if((he = gethostbyname(argv[1])) == NULL){
+		perror("gethostbyname error");
+		exit(-1);
+	}
+
+	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) == -1){
+		perror("create socket error");
+		exit(-1);
+	}
+
+	//fill 
+	bzero(&server,sizeof(server));
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORT);
+	server.sin_addr = *((struct in_addr*)he->h_addr);
+
+	//connect
+	if(connect(sockfd,(struct sockaddr *)&server,sizeof(server)) == -1){
+		perror("connect error");
+		exit(-1);
+	}
+	process(stdin,sockfd);
+	close(sockfd);
+
+	return 0;
+}
